fe58a1501a57483ed2283643e0f479297a31d0ce
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-10-13 09:52:04 -0700

    Carousel on Homepage
    Yay for DIY things that people usually just use reactstrap or something to accomplish.

---
+++ front-end/src/data/data.js
@@ -0,0 +1,53 @@
+export const packages = [
+  {
+    name: "JavaScript",
+    image:
+      "https://png2.kisspng.com/sh/96f8ccf8bd656a88650686444119aa58/L0KzQYm3VcE0N6R4iZH0aYP2gLBuTfpifpJ4eARycISwfrFrhb1re15xh9l4LXPyfcH8lPVzNaF3h9l7YX3webBuTgBzd5h3RadqZki6QbO7gfRnPmM3RqMEN0G6SIWBUcUzPmM4SaYCNke0Qoe1kP5o/kisspng-javascript-node-js-logo-computer-programming-progr-5af871b4adf622.1971784815262314767126.png",
+  },
+  {
+    name: "Node",
+    image:
+      "https://png2.kisspng.com/sh/87a14e16cbc0612b26b3b2925e496a58/L0KzQYm3VsE6N6l9jZH0aYP2gLBuTf5wbJYyguU2anH5ccTqkvlxfF5qkOJ7ZYP2Pbv6TgNmeqdqip98aXToPcTqkvlxfJpzRdD4ZHWwesS0VfIzQJc7SaU8Y0a2cYW1VsQyO2k8Sac6NUK8RIK4UMkyOmU8TJD5bne=/kisspng-node-js-javascript-express-js-server-side-scriptin-node-js-5b28f6133c63a4.6413871515294110912474.png",
+  },
+  {
+    name: "React",
+    image: "https://logosrated.net/wp-content/uploads/parser/React-Logo-1.png",
+  },
+  {
+    name: "Redux",
+    image: "https://cdn-images-1.medium.com/max/1200/0*U2DmhXYumRyXH6X1.png",
+  },
+  {
+    name: "Mongo",
+    image:
+      "https://png2.kisspng.com/sh/1f2cc46c8ad64f5cf78723046f873c32/L0KzQYm3V8AzN6lsfpH0aYP2gLBuTf1wdph0fNQ2ZHH3cbPokBUudpD4id42cHB2hLj5hgNydF5yh9Dwbz24coSChslmO2dnSao6ND6zQ4W7V8MyOWI6S6IAMka0R4q7U8g4NqFzf3==/kisspng-mongodb-database-nosql-postgresql-mongo-5b39f9e36b1814.0344731115305261794387.png",
+  },
+  {
+    name: "Axios",
+    image: "http://upload.wikimedia.org/wikipedia/en/thumb/4/41/Axioslogo.png/500px-Axioslogo.png",
+  },
+  {
+    name: "Mocha",
+    image:
+      "https://camo.githubusercontent.com/af4bf83ab2ca125346740f9961345a24ec43b3a9/68747470733a2f2f636c6475702e636f6d2f78465646784f696f41552e737667",
+  },
+  {
+    name: "Chai",
+    image: "https://avatars2.githubusercontent.com/u/1515293?s=400&v=4",
+  },
+  {
+    name: "SQL",
+    image:
+      "https://png2.kisspng.com/sh/0e7a4c66da27ad4c765eda5c33738e16/L0KzQYm3U8IxN6tuiZH0aYP2gLBuTgRiap1qReJ7b3f1cb70hgIue6JxReVubHXxecb0TfNwdaF6jNd7LYPydsXAggJmNaRuktc2aXPyfn76kfwue5Z3jtd7LUXkcoHqhcEzOGM2fdg5LkW6QYO5WcMxOWY3Sac8NkW2QIG3WMcveJ9s/kisspng-table-programmer-sql-selenium-computer-software-size-icon-sql-server-5ab0ce12021ef0.5712293015215365300087.png",
+  },
+  {
+    name: "Python",
+    image:
+      "https://png2.kisspng.com/sh/38750e59bd98d512116306aa7bcebd8c/L0KzQYm3U8I2N6puiZH0aYP2gLBuTgB6fJl0hp91b3fyPcH5jBdzaZ5yfeQ2ZnnogrTsTgB6fJl0hp9sbHnzccP7k702aZM8etZuMkC5drOAUb46PGc3SqY9NEG4QoKCWcEyO2k1SqoBLoDxd1==/kisspng-python-logo-programmer-fierce-python-cliparts-5ab7bde206fb71.9462244415219911380286.png",
+  },
+  {
+    name: "Django",
+    image:
+      "https://png2.kisspng.com/sh/e7fe785bce4f5309a6675d504c7e3eef/L0KzQYm3V8EyN5Rwe5H0aYP2gLBuTfRraZ9sh59CZXKwdLb9hfxweJ5qhuY2d3XlPbf5gf1mf5D3g595eYTrf7A0kB9nfKhmRdZzYX7qf368gsQ2bGo2TKQCNHW3RnA3VcU4PGY6T6MAM0G2QIW5UcIyPmIziNDw/kisspng-django-web-development-web-framework-python-softwa-django-5b45d914274e46.055745571531304212161.png",
+  },
+];
