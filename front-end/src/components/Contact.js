3f2bcfd853347c7ad19de4bcbe99b9fd64fd420d
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-10-15 16:25:27 -0700

    Contact Component Written/Styled
    Need to hook up backend and mailchimp or whichever way I want to go about with the backend.

--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -4,10 +4,53 @@ import { connect } from "react-redux";
 import "../css/Contact.css";
 
 class Contact extends Component {
+  state = {
+    name: "",
+    title: "",
+    body: "",
+    email: "",
+    copy: false,
+  };
+
+  handleInput = event => {
+    console.log("e", event.target.name);
+    this.setState({ [event.target.name]: event.target.value });
+  };
   render() {
+    let { name, title, body, email, copy } = this.state;
     return (
       <div className="contact">
-        Contact
+        <form className="contact__form">
+          <input name="name" value={name} className="form__input" placeholder="Your Name" onChange={this.handleInput} />
+          <input
+            name="email"
+            value={email}
+            className="form__input"
+            placeholder="Your Email"
+            onChange={this.handleInput}
+          />
+          <input
+            name="title"
+            value={title}
+            className="form__input"
+            placeholder="Title of Message"
+            onChange={this.handleInput}
+          />
+          <textarea
+            name="body"
+            value={body}
+            className="form__textarea"
+            placeholder="Your Message"
+            onChange={this.handleInput}
+          />
+          <div className="form__check">
+            <label className="check__label">Would you like a copy of this message?</label>
+            <input type="checkbox" name="copy" value={copy} className="check__box" onChange={this.handleInput} />
+          </div>
+          <button type="submit" className="form__button">
+            <i class="fas fa-envelope fa-2x button__icon" />
+          </button>
+        </form>
       </div>
     );
   }
