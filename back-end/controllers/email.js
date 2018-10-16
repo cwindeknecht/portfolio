8e9d71217250c07cb85b68306733220cba9d31e1
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-10-16 10:35:16 -0700

    Styled Contact, Added Error Handling, Setup Backend
    Frontend is now communicating with backend.  Just need to set up the email service.

---
+++ back-end/controllers/email.js
@@ -0,0 +1,9 @@
+const axios = require("axios");
+
+const sendEmails = (req, res) => {
+    console.log(req)
+};
+
+module.exports = {
+  sendEmails,
+};
--- back-end/routes.js
+++ back-end/routes.js
@@ -1,5 +1,7 @@
 module.exports = app => {
   const pexelController = require("./controllers/pexel");
+  const emailController = require("./controllers/email");
 
   app.route("/randompicture/:query").get(pexelController.getRandom);
+  app.route("/email/:copy").post(emailController.sendEmails);
 };
