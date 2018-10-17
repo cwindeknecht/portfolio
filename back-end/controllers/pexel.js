df04788a12d8dd82ae390de4b4f8be8e00785d71
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-10-17 13:34:39 -0700

    Random Pictures Working
     Component styled, hitting Pexels API.  Defintitely not finished, but a decent start

--- back-end/controllers/pexel.js
+++ back-end/controllers/pexel.js
@@ -1,21 +1,30 @@
-const axios = require("axios");
+const PexelsAPI = require("pexels-api-wrapper");
 
-let random = query => {
-  return {
-    method: "GET",
-    url: `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`,
-    headers: {
-      Authorization: process.env.REACT_APP_PEXELS || process.env.HEROKU_PEXELS,
-    },
-  };
+var pexelsClient = new PexelsAPI(process.env.REACT_APP_PEXELS || process.env.HEROKU_PEXELS);
+
+const getPictures = (req, res) => {
+  pexelsClient
+    .search(req.params.query, req.params.query.length, 1)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
 };
 
-const getRandom = (req, res) => {
-  axios(random(req.params.query))
-    .then(results => console.log(results))
-    .catch(err => console.log(err));
+const getPicture = (req, res) => {
+  pexelsClient
+    .search(req.params.query, 1, 1)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
 };
 
 module.exports = {
-  getRandom,
+  getPictures,
+  getPicture,
 };
